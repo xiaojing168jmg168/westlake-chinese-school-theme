5909a244fa83a0486f6b3d519e4ca35bd3d1231d
Xiaojing Deng <8redfishcreative@gmail.com>
2023-04-17 11:38:34 -0400

    dynamic wordpress menu or custom menu

--- footer.php
+++ footer.php
@@ -12,6 +12,11 @@
             <div class="site-footer__col-two">
               <h3 class="headline headline--small">Explore</h3>
               <nav class="nav-list">
+                <!-- <?php
+                wp_nav_menu(array(
+                  'theme_location' => 'footerLocationOne'
+                ))
+                ?> -->
                 <ul>
                   <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                   <li><a href="#">Programs</a></li>
@@ -24,6 +29,11 @@
             <div class="site-footer__col-three">
               <h3 class="headline headline--small">Learn</h3>
               <nav class="nav-list">
+              <!-- <?php
+                wp_nav_menu(array(
+                  'theme_location' => 'footerLocationTwo'
+                ))
+                ?> -->
                 <ul>
                   <li><a href="#">Legal</a></li>
                   <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
@@ -36,6 +46,7 @@
           <div class="site-footer__col-four">
             <h3 class="headline headline--small">Connect With Us</h3>
             <nav>
+           
               <ul class="min-list social-icons-list group">
                 <li>
                   <a href="#" class="social-color-facebook"
