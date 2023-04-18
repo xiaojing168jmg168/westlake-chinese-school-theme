31e3d6153c66d57635c00ff0c3c0962c2c31d515
Xiaojing Deng <8redfishcreative@gmail.com>
2023-04-18 19:18:08 -0400

    add some button function

--- front-page.php
+++ front-page.php
@@ -95,7 +95,7 @@ while($homepagePosts->have_posts()) {
        
 
           <p class="t-center no-margin">
-            <a href="#" class="btn btn--yellow">View All Blog Posts</a>
+            <a href="<?php echo site_url('/blog')?>" class="btn btn--yellow">View All Blog Posts</a>
           </p>
         </div>
       </div>
