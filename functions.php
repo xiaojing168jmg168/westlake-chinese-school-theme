0d96450cc3e938697806c0e44fa90cf3e349025d
Xiaojing Deng <8redfishcreative@gmail.com>
2023-04-21 22:31:55 -0400

    add past events page

--- functions.php
+++ functions.php
@@ -17,3 +17,22 @@ function wcs_features(){
 }
 
 add_action('after_setup_theme', 'wcs_features');
+
+function wcs_adjust_queries($query) {
+  if(!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()){
+    $today = date('Ymd');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query',array(
+        array(
+          'key' => 'event_date',
+          'compare' => '>=',
+          'value' => $today,
+          'type' => 'numeric'
+        )
+        ));
+  }
+}
+
+add_action('pre_get_posts', 'wcs_adjust_queries');
