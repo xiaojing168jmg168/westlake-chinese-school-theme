935596041820e173036b997c28f11bcf1e6a425f
Xiaojing Deng <8redfishcreative@gmail.com>
2023-04-21 21:41:57 -0400

    set event order

--- front-page.php
+++ front-page.php
@@ -23,16 +23,32 @@
           <h2 class="headline headline--small-plus t-center">
             Upcoming Events
           </h2>
-<?php $homepageEvents = new WP_Query(array(
-  'posts_per_page' => 2,
-  'post_type' => 'event'
+<?php 
+$today = date('Ymd');
+$homepageEvents = new WP_Query(array(
+  'posts_per_page' => -1,
+  'post_type' => 'event',
+  'meta_key' => 'event_date',
+  'orderby' => 'meta_value_num',
+  'order' => 'ASC',
+  'meta_query' => array(
+    array(
+      'key' => 'event_date',
+      'compare' => '>=',
+      'value' => $today,
+      'type' => 'numeric'
+    )
+  )
 ));
 while($homepageEvents->have_posts()) {
 $homepageEvents->the_post(); ?>
   <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Mar</span>
-              <span class="event-summary__day">25</span>
+              <span class="event-summary__month"><?php 
+              $eventDate = new DateTime(get_field('event_date'));
+              echo $eventDate->format('M')
+              ?></span>
+              <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny">
