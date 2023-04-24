906aae29955add6391c4305c9584ad6376dc59c6
Xiaojing Deng <8redfishcreative@gmail.com>
2023-04-24 11:22:40 -0400

    add program page, set relationship between program and event

--- front-page.php
+++ front-page.php
@@ -13,7 +13,7 @@
           Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re
           interested in?
         </h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+        <a href="<?php echo get_post_type_archive_link('program') ?>" class="btn btn--large btn--blue">Find Your Major</a>
       </div>
     </div>
 
