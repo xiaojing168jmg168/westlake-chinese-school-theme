bd952dbff8235b1053fe5ef334e750f096d60fb4
Xiaojing Deng <8redfishcreative@gmail.com>
2023-04-26 14:09:40 -0400

    create page banner function, reduce duplicate code

--- functions.php
+++ functions.php
@@ -1,4 +1,36 @@
 <?php
+function pageBanner($args = [
+  'title' => null,
+  'subtitle' => null,
+  'image' => null
+]) {
+if (!$args['title']) {
+$args['title'] = get_the_title();
+}
+
+if (!$args['subtitle']) {
+$args['subtitle'] = get_field('page_banner_subtitle');
+}
+if (!$args['image']) {
+  if(get_field('page_banner_background_image')){
+    $args['image'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+  }
+ else{
+  $args['image'] = get_theme_file_uri('/images/ocean.jpg');
+ }
+  }
+  ?>
+  <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['image']; ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+        <div class="page-banner__intro">
+          <p><?php echo $args['subtitle'] ?></p>
+        </div>
+      </div>
+    </div>
+  <?php
+}
 function wcs_files(){
     wp_enqueue_script('main-chinese-js',get_theme_file_uri('/build/index.js'),array('jquery'), '1.0', true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
