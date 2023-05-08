d3fe0c5a4637daf2aab2618d83a0647a37c9deba
Xiaojing Deng <8redfishcreative@gmail.com>
2023-05-08 16:30:13 -0400

    completing search overlay

--- inc/search-route.php
+++ inc/search-route.php
@@ -43,7 +43,8 @@ function wcsSearchResults($data){
     if(get_post_type() == 'program'){
         array_push($results['programs'], array(
             'title' => get_the_title(),
-            'permalink' => get_the_permalink()
+            'permalink' => get_the_permalink(),
+            'id' => get_the_id()
         ));
     }
    
@@ -64,5 +65,52 @@ function wcsSearchResults($data){
         ));
     }
   }
+  if($results['program']){
+    $programsMetaQuery = array('relation' => 'OR');
+
+    foreach($results['programs'] as $item) {
+      array_push($programsMetaQuery, array(
+          'key' => 'related_programs',
+          'compare' => 'LIKE',
+          'value' => '"' . $item['id'] . '"'
+        ));
+    }
+     $programRelationshipQuery = new WP_Query(array(
+      'post_type' => array('instructor', 'event'),
+      'meta_query' => $programsMetaQuery
+          ));
+    while($programRelationshipQuery->have_posts()){
+              $programRelationshipQuery->the_post();
+              if (get_post_type() == 'event') {
+                $eventDate = new DateTime(get_field('event_date'));
+                $description = null;
+                if (has_excerpt()) {
+                  $description = get_the_excerpt();
+                } else {
+                  $description = wp_trim_words(get_the_content(), 18);
+                }
+        
+                array_push($results['events'], array(
+                  'title' => get_the_title(),
+                  'permalink' => get_the_permalink(),
+                  'month' => $eventDate->format('M'),
+                  'day' => $eventDate->format('d'),
+                  'description' => $description
+                ));
+              }
+              if(get_post_type() == 'instructor'){
+                  array_push($results['instructors'], array(
+                      'title' => get_the_title(),
+                      'permalink' => get_the_permalink(),
+                      'image' => get_the_post_thumbnail_url()
+                  ));
+              }
+
+          }
+          //remove duplicates
+  $results['instructors'] = array_values(array_unique($results['instructors'], SORT_REGULAR));
+  $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
+  }
+  
   return $results;
 }
--- single-program.php
+++ single-program.php
@@ -10,7 +10,7 @@ pageBanner();
           <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
-        <div class="generic-content"><?php the_content(); ?></div>
+        <div class="generic-content"><?php the_field('main_body_content'); ?></div>
         <?php 
 $relatedInstructors = new WP_Query(array(
   'posts_per_page' => -1,
