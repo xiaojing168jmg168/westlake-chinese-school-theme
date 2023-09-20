78b78aeaf6cb1a005c55b6bf8a5fa4b526f9103f
Xiaojing Deng <8redfishcreative@gmail.com>
2023-09-20 10:57:55 -0400

    fix styles

--- single-instructor.php
+++ single-instructor.php
@@ -9,7 +9,40 @@ pageBanner();
         <div class="generic-content">
             <div class="row group">
                 <div class="one-third"><?php the_post_thumbnail(); ?></div>
-                <div class="two-third"><?php the_content(); ?></div>
+                <div class="two-third">
+                    <?php 
+                      $likeCount = new WP_Query(array(
+                        'post_type' => 'like',
+                        'meta_query' => array(
+                            array(
+                                'key' => 'liked_instructor_id',
+                                'compare' => '=',
+                                'value' => get_the_ID()
+                            )
+                        )
+                            ));
+                      $existQuery = 'no';
+                      $existQuery = new WP_Query(array(
+                        'author' => get_current_user_id(),
+                        'post_type' => 'like',
+                        'meta_query' => array(
+                            array(
+                                'key' => 'liked_instructor_id',
+                                'compare' => '=',
+                                'value' => get_the_ID()
+                            )
+                        )
+                            ));
+                            if($existQuery->found_posts){
+                                $existQuery = 'yes';
+                            }
+                    ?>
+                    <span class="like-box" data-exists="<?php echo $existStatus; ?>">
+                       <i class="fa fa-heart-o" aria-hidden="true"></i>
+                       <i class="fa fas" aria-hidden="true"></i>
+                       <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
+                    </span>
+                    <?php the_content(); ?></div>
             </div>
         </div>
        <?php
